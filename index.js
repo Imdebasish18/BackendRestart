d0c7fcaef16cbb082efeb4ac6075024366b38cac
Imdebasish18 <study4me24.7@gmail.com>
2025-06-12 03:57:36 +0530

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/user", (req, res) => {
+  res.send("Welcome Debasish!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
